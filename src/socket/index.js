b7a161c6c4544f009743c6a7ea39338c0e23767d
hyoni0817 <hyoni0817@naver.com>
2019-03-27 19:30:08 +0900

    socket 연결하는 소스코드

---
+++ src/socket/index.js
@@ -0,0 +1,11 @@
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:5000");
+
+function connect(cb) {
+    socket.on("chat", message => {
+        console.log(message);
+        cb(message);
+    });
+}
+
+export {connect};
