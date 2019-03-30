03c2e9f7c9fba0ec1cf1246acc1275ec0ff2537b
hyoni0817 <hyoni0817@naver.com>
2019-03-30 23:54:05 +0900

    메세지를 보내고, 주고 받은 메세지 리스트를 보이게 하는 소스코드 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-import {connect} from './socket';
+import { msgOn, msgEmit } from './socket';
 
 
 
 class App extends Component {
+  input = React.createRef();
+  state = {
+    message : '',
+    msgData : []
+  }
 
   componentDidMount(){
-    connect(message => {
-      console.log(message);
+    //const {msgData} = this.state; 
+    //이곳에 선언하면 메세지가 쌓이지 못하고 새로운 내용으로 계속 교체된다.
+    msgOn(message => {
+      // 리스트로 메세지 보여주기 위한 코드
+      const {msgData} = this.state; 
+
+      this.setState({
+        msgData:msgData.concat(message)
+      })
     });
   }
 
-  render() {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    msgEmit(this.state.message);
 
+    this.setState({ message : '' });
+    this.input.current.focus();
+  }
+  render() {
+    const {msgData} = this.state;
+    const msgList = msgData.map((msg, idx) => (
+      <p key={idx}>{msg}</p>
+    ))
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div>
+          {msgList}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input id="contents" autoComplete="off" value={this.state.message} onChange={e => this.setState({ message: e.target.value})} ref={this.input}/><button>Send</button>
+        </form>
+      </Fragment>
     );
   }
 }
