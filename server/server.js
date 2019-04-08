4f3ce0c2b517d0f89ebf30e2fc4f6fc6c3cadb5c
hyoni0817 <hyoni0817@naver.com>
2019-04-09 02:08:38 +0900

    보낸 메세지와 받은 메세지를 구분하기 위해 메세지 전송을 broadcast로 수정함

--- server/server.js
+++ server/server.js
@@ -6,8 +6,16 @@ const io = require("socket.io")(server);
 io.set("origins", "*:*");
 io.on("connection", socket => {
     console.log("Client Successfully Connected");
+
     socket.on('chat', msg => { // msgEmit에서 메세지 받기
-        io.sockets.emit("chat", msg); 
+
+        var output = {userid : socket.id, msg : msg};
+
+        //나를 제외하고 메세지 전송하기
+        socket.broadcast.emit("chat", output)
+
+        //나를 포함해서 메세지 전송하기
+        //io.sockets.emit("chat", output); 
     })
 })
 
