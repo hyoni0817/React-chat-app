df452f4f753659399f1942d46fc4f9de75367ff7
hyoni0817 <hyoni0817@naver.com>
2019-04-14 04:10:11 +0900

    user 정보를 전달하는 소스 코드 추가

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,13 +4,21 @@ const socket = openSocket("http://localhost:5000");
 //메세지 받기
 function msgOn(cb) { 
     socket.on('chat', message => {
-        cb(message);
+        cb(message.msg);
     });
 }
 
+//유저 정보 전달
+function userView(cb) {
+    socket.on('entrace', message => {
+        const output = {userId : message.userId, participation: message.participation}
+        cb(output);
+   
+    })
+}
 //메세지 보내기
 function msgEmit(msg) {
     socket.emit('chat', msg);
 }
 
-export {msgOn, msgEmit};
+export {msgOn, msgEmit, userView};
