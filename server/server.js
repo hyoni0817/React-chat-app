9b83383a734d971b8369853f16cd117a3015360d
hyoni0817 <hyoni0817@naver.com>
2019-04-14 04:09:02 +0900

    user의 정보를 나타내고, 클라이언트에게 user 정보 전달하는 소스코드 추가

--- server/server.js
+++ server/server.js
@@ -2,15 +2,27 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+let i = 0;
+let user = [];
 
 io.set("origins", "*:*");
 io.on("connection", socket => {
     console.log("Client Successfully Connected");
 
-    socket.on('chat', msg => { // msgEmit에서 메세지 받기
+    let userInfo = {};
+    userInfo.socketId = socket.id;
+    userInfo.userId = '방랑자' + (++i);
+    userInfo.participation = 'Y';
+    user.push(userInfo);
 
+    io.sockets.emit("entrace", userInfo); //user정보 보내기
+
+    
+    socket.on('chat', msg => { // msgEmit에서 메세지 받기
+        
+        var output = {msg : msg, id : userInfo.userId};
         //나를 제외하고 메세지 전송하기
-        socket.broadcast.emit("chat", msg)
+        socket.broadcast.emit("chat", output)
 
         //나를 포함해서 메세지 전송하기
         //io.sockets.emit("chat", output); 
