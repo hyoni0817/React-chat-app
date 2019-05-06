bba05bc472f9611e49967c915efc536f0c1be098
hyoni0817 <hyoni0817@naver.com>
2019-05-07 04:28:17 +0900

    메세지 박스 크기에 상관없이 시간이 하단에 위치 할 수 있도록 수정.

--- src/App.js
+++ src/App.js
@@ -129,16 +129,17 @@ class App extends Component {
       const msgList = msgData.map((msg, idx) => {
         let msgStat = '', msgBox = '', userView = '';
         function timeView(msgTime) {
-          return ( msgData[idx+1] !== undefined && (msgData[idx+1].id === msg.id) && (msgData[idx+1].time === msg.time) ? '' : <p className={msgTime}>{msg.time}</p>);
+          return ( msgData[idx+1] !== undefined && (msgData[idx+1].id === msg.id) && (msgData[idx+1].time === msg.time) ? '' : <span className={msgTime}>{msg.time}</span>);
         }
 
         userView = (idx !== 0 && (msg.time !== msgData[idx-1].time) ? <p className="user-id"><strong>{msg.id}</strong></p> : (idx !== 0 && (msg.id !== msgData[idx-1].id) ? <p className="user-id"><strong>{msg.id}</strong></p> : ''));
 
         if(msg.sysmsg === undefined) {
           msgStat = (msg.myMsg !== 'Y' ? "other-msg" : "my-msg");
-          msgBox = ( msgStat === 'other-msg' ? <div key={++id}>{userView}<div className={msgStat}><p className="msg">{msg.message}</p></div>{timeView('other-msg-time')}</div> : <div key={++id}><div className={msgStat}><p className="msg">{msg.message}</p></div>{timeView('my-msg-time')}</div>)
+          msgBox = ( msgStat === 'other-msg' ? <div key={++id}>{userView}<div className={msgStat}><p className="msg">{msg.message}</p>{timeView('other-msg-time')}</div></div> : <div key={++id}><div className={msgStat}><p className="msg">{msg.message}</p>{timeView('my-msg-time')}</div></div>)
 
           return msgBox;
+          
         } else {
             if(msg.sysmsg === 'entrance') {
               return (myNick === msg.userId ? <div key={++id}><p className="my-entrace"><strong>{msg.userId}(나)님</strong>이 입장했습니다.</p></div> : <div key={++id}><p id="user-entrace"><strong>{msg.userId}님</strong>이 입장했습니다.</p></div>)
